cec6b196ed3673cf2445a22985df57a5b555ab69
chubyqc <chuby.qc@gmail.com>
2019-04-27 17:24:19 -0400

    Update marie-moi.js

--- mariage/arianeetjimmy/marie-moi.js
+++ mariage/arianeetjimmy/marie-moi.js
@@ -1,8 +1,38 @@
 mariemoi = {
-    init(password, wrongPasswordUrl) {
+    emailToken: null,
+    
+    init(password, wrongPasswordUrl, emailToken) {
         var userPassword = prompt('Mot de passe?');
         if (userPassword != password) {
             window.location.href = wrongPasswordUrl;
         }
+        this.emailToken = emailToken;
+    },
+    
+    /**
+     * answer looks like this:
+     *  {
+     *      isComing: true,
+     *      names: ['name1', 'name2'],
+     *      sleeps: {
+     *          friday: false,
+     *          saturday: true,
+     *          sunday: true,
+     *          monday: false
+     *      },
+     *      song: 'Some dancing song',
+     *      moreInfo: 'more info left by the user'
+     *  }
+     */
+    confirm(answer, from, to, subject) {
+        Email.send({
+            SecureToken : this.emailToken,
+            To : to,
+            From : from || to,
+            Subject : subject,
+            Body : JSON.stringify(answer)
+        }).then(
+            message => console.log(message)
+        );
     }
 }
